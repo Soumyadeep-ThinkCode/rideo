ae8e2644bcd8655377f3d93a242a8284661b072c
Soumyadeep Ghosh <soumyadeep.sam@gmail.com>
2017-11-11 23:14:37 +0530

    4th Test

---
+++ server.js
@@ -0,0 +1,28 @@
+var app = require('./app');
+var port = process.env.PORT || 8080;
+
+var server = app.listen(port, function(){
+  console.log('Express server listening on port ' + port);
+});
+
+var io = require('socket.io')(server);
+app.io = io;
+
+var mysocket;
+
+io.on('connection',function(socket){
+  mysocket = socket;
+  console.log('hello');
+  socket.emit('news', {hello : 'world'});
+  socket.on('other event',function(data){
+    console.log(data);
+  });
+});
+
+app.use('/changeMarker', function(req, res){
+  mysocket.emit('test', 'changeMarker');
+  res.status(200).send('OK');
+});
+
+
+// module.exports = mysocket;
